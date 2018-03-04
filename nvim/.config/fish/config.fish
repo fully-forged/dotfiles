cf030c28e89ec2ae63326c5b252c85cbf742032e
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-03-04 14:42:14 +0000

    Adds memento aliases

--- nvim/.config/fish/config.fish
+++ nvim/.config/fish/config.fish
@@ -6,6 +6,8 @@ set -x PATH /usr/local/sbin $PATH
 set -x FZF_DEFAULT_COMMAND 'rg --files'
 set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 
+set -x MEMENTO_BASE_URL 'https://memento.fullyforged.com'
+
 source ~/.kiex/scripts/kiex.fish
 source $HOME/.kiex/elixirs/.elixir-1.6.1.env.fish
 
@@ -27,6 +29,10 @@ end
 
 function cr
   git remote prune origin
+end
+
+function m
+  memento | less -r
 end
 
 eval (direnv hook fish)
